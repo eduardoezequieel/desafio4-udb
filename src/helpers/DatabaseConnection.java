2224438b52d3329c1c8fdf6e2b0a68c6e0ed0b39
Eduardo Ezequiel <57075508+eduardoezequieel@users.noreply.github.com>
2023-11-12 23:13:00 -0600

    fix: add better indenting to DatabaseConnection file

--- src/helpers/DatabaseConnection.java
+++ src/helpers/DatabaseConnection.java
@@ -1,29 +1,23 @@
-
 package helpers;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class DatabaseConnection {
-    private String user= "root";
-    private String password= "";
-    private String url="jdbc:mysql://localhost:3306/library";
+    private String user = "root";
+    private String password = "";
+    private String url = "jdbc:mysql://localhost:3306/library";
     private Connection connection;
     
     public DatabaseConnection(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection=DriverManager.getConnection(url, user, password);
-            
-            
-            
+            connection = DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
-            System.out.println("Error al conectarse a la base de datos"+e.getMessage());
-            
+            System.out.println("Error al conectarse a la base de datos" + e.getMessage());
         }
-        
     } 
+    
     public Connection getConnection(){
         return connection;
     }
