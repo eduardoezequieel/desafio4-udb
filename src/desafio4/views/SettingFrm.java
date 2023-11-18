58555a128683e1f653f20239de0b24899eea483f
Diego Esnard <Diegoesnard10@gmail.com>
2023-11-17 22:40:50 -0600

    CorregidoJFramedeSetting
    Ya nuevo

--- src/desafio4/views/SettingFrm.java
+++ src/desafio4/views/SettingFrm.java
@@ -13,18 +13,18 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import models.UserType;
 
+public class SettingFrm extends javax.swing.JPanel {
 
-
-public class SettingsFrm extends javax.swing.JFrame {
-
-    private Object Jtable1;
+   private Object Jtable1;
 
   
-    public SettingsFrm()  { 
+    public SettingFrm()  { 
     initComponents();
     mostrarUserTypesEnJTable(getTwoUsergetTypes());
+    
+    
 }
-private List<UserType> getTwoUsergetTypes() {
+    private List<UserType> getTwoUsergetTypes() {
  List<UserType> twoUserTypes = new ArrayList<>();
         
         try {
@@ -71,14 +71,10 @@ private List<UserType> getTwoUsergetTypes() {
     jTable1.setModel(model);
 }
  
-        
- 
- 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -90,19 +86,17 @@ private List<UserType> getTwoUsergetTypes() {
         jButton1 = new javax.swing.JButton();
         jPanel4 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        cantidadPrestamosTxt1 = new javax.swing.JTextField();
+        valorEjemplarTxt = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        cantidadPrestamosTxt2 = new javax.swing.JTextField();
+        tarifaTxt = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jPanel1.setPreferredSize(new java.awt.Dimension(1090, 640));
-        jPanel1.setVerifyInputWhenFocusTarget(false);
+        setPreferredSize(new java.awt.Dimension(1060, 640));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
         jPanel2.setMinimumSize(new java.awt.Dimension(1090, 640));
         jPanel2.setName(""); // NOI18N
-        jPanel2.setPreferredSize(new java.awt.Dimension(1090, 640));
+        jPanel2.setPreferredSize(new java.awt.Dimension(1060, 640));
         jPanel2.setVerifyInputWhenFocusTarget(false);
 
         jPanel3.setBackground(new java.awt.Color(245, 251, 248));
@@ -193,10 +187,10 @@ private List<UserType> getTwoUsergetTypes() {
         jLabel3.setForeground(new java.awt.Color(154, 168, 180));
         jLabel3.setText("Valor de cada ejemplar");
 
-        cantidadPrestamosTxt1.setText("$50.00");
-        cantidadPrestamosTxt1.addActionListener(new java.awt.event.ActionListener() {
+        valorEjemplarTxt.setText("$50.00");
+        valorEjemplarTxt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cantidadPrestamosTxt1ActionPerformed(evt);
+                valorEjemplarTxtActionPerformed(evt);
             }
         });
 
@@ -204,10 +198,20 @@ private List<UserType> getTwoUsergetTypes() {
         jLabel4.setForeground(new java.awt.Color(154, 168, 180));
         jLabel4.setText("Tarifa de mora diaria %");
 
-        cantidadPrestamosTxt2.setText("0.10");
-        cantidadPrestamosTxt2.addActionListener(new java.awt.event.ActionListener() {
+        tarifaTxt.setText("0.05");
+        tarifaTxt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cantidadPrestamosTxt2ActionPerformed(evt);
+                tarifaTxtActionPerformed(evt);
+            }
+        });
+
+        jButton2.setBackground(new java.awt.Color(82, 190, 127));
+        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("Actualizar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
             }
         });
 
@@ -217,12 +221,14 @@ private List<UserType> getTwoUsergetTypes() {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addGap(265, 265, 265)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(cantidadPrestamosTxt1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4)
-                    .addComponent(cantidadPrestamosTxt2, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton2)
+                    .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel3)
+                        .addComponent(valorEjemplarTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel4)
+                        .addComponent(tarifaTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(537, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -230,12 +236,14 @@ private List<UserType> getTwoUsergetTypes() {
                 .addGap(25, 25, 25)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(cantidadPrestamosTxt1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(valorEjemplarTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(cantidadPrestamosTxt2, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(131, Short.MAX_VALUE))
+                .addComponent(tarifaTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addContainerGap(84, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -243,10 +251,10 @@ private List<UserType> getTwoUsergetTypes() {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(19, 19, 19)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
@@ -254,133 +262,81 @@ private List<UserType> getTwoUsergetTypes() {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(98, Short.MAX_VALUE))
+                .addContainerGap(94, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 1043, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 651, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1060, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 1060, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 662, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
-
-        pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void cantidadPrestamosTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cantidadPrestamosTxtActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_cantidadPrestamosTxtActionPerformed
 
-    private void cantidadPrestamosTxt1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cantidadPrestamosTxt1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cantidadPrestamosTxt1ActionPerformed
-
-    private void cantidadPrestamosTxt2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cantidadPrestamosTxt2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cantidadPrestamosTxt2ActionPerformed
-
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-       String userTypeName = usuarioTypeCb.getSelectedItem().toString();
-    int nuevosMateriales;
+        String userTypeName = usuarioTypeCb.getSelectedItem().toString();
+        int nuevosMateriales;
 
-    try {
-        nuevosMateriales = Integer.parseInt(cantidadPrestamosTxt.getText());
-
-        if (nuevosMateriales < 0) {
-            JOptionPane.showMessageDialog(null, "La cantidad de materiales no puede ser negativa.");
-            return;
-        }
-
-        // Asegúrate de crear una instancia de la clase PermisosCrtl antes de usarla
-        PermisosCtrl permisosCtrl = new PermisosCtrl();
-        
-        // Corrige el nombre del método a actualizarMaterialesPermitidos
-        permisosCtrl.actualizarMaterialesPermitidos(userTypeName, nuevosMateriales);
-        
-        // Asegúrate de que permisosCtrl.getTwoUsergetTypes() devuelva una lista válida de UserType
-        mostrarUserTypesEnJTable(permisosCtrl.getTwoUsergetTypes());
-
-    } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido para la cantidad de materiales.");
-    }
-    }//GEN-LAST:event_jButton1MouseClicked
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SettingsFrm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SettingsFrm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SettingsFrm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SettingsFrm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+            nuevosMateriales = Integer.parseInt(cantidadPrestamosTxt.getText());
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SettingsFrm().setVisible(true);
+            if (nuevosMateriales < 0) {
+                JOptionPane.showMessageDialog(null, "La cantidad de materiales no puede ser negativa.");
+                return;
             }
-        });
-    }
+
+            // Asegúrate de crear una instancia de la clase PermisosCrtl antes de usarla
+            PermisosCtrl permisosCtrl = new PermisosCtrl();
+
+            // Corrige el nombre del método a actualizarMaterialesPermitidos
+            permisosCtrl.actualizarMaterialesPermitidos(userTypeName, nuevosMateriales);
+
+            // Asegúrate de que permisosCtrl.getTwoUsergetTypes() devuelva una lista válida de UserType
+            mostrarUserTypesEnJTable(permisosCtrl.getTwoUsergetTypes());
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido para la cantidad de materiales.");
+        }
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void valorEjemplarTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_valorEjemplarTxtActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_valorEjemplarTxtActionPerformed
+
+    private void tarifaTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tarifaTxtActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tarifaTxtActionPerformed
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2MouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField cantidadPrestamosTxt;
-    private javax.swing.JTextField cantidadPrestamosTxt1;
-    private javax.swing.JTextField cantidadPrestamosTxt2;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTextField tarifaTxt;
     private javax.swing.JComboBox<String> usuarioTypeCb;
+    private javax.swing.JTextField valorEjemplarTxt;
     // End of variables declaration//GEN-END:variables
-
-    
 }
- 
-
