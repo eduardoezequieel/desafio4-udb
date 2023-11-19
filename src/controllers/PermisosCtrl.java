2d91862a07c02b6756098c0a240307dddaa4aa62
Diego Esnard <Diegoesnard10@gmail.com>
2023-11-19 00:10:19 -0600

    Al fin co;o
    Bien que zi

--- src/controllers/PermisosCtrl.java
+++ src/controllers/PermisosCtrl.java
@@ -17,6 +17,8 @@ import models.UserType;
 
 public class PermisosCtrl {
     public PermisosCtrl(){}
+    public javax.swing.JTable jTable2;
+
     
     public List<UserType> getTwoUsergetTypes() {
         List<UserType> twoUserTypes = new ArrayList<>();
@@ -111,6 +113,105 @@ public class PermisosCtrl {
 
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Hubo un problema al actualizar los materiales permitidos. Por favor, contacte con el administrador.");
+            System.out.println(e.getMessage());
+        }
+    }
+public List<Object[]> obtenerResultadosYearlyOverdue() {
+    List<Object[]> results = new ArrayList<>();
+
+    try {
+        // Lógica para ejecutar la consulta y obtener los resultados
+        DatabaseConnection dbcn = new DatabaseConnection();
+        Connection cn = dbcn.getConnection();
+
+        String sql = "SELECT cost, year FROM yearlyOverdue;";
+        try (Statement st = cn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Object[] resultRow = {rs.getObject("cost"), rs.getObject("year")};
+                results.add(resultRow);
+                
+            }
+        rs.close();
+        cn.close();
+        }
+
+       
+
+    } catch (Exception e) {
+        // Manejo de excepciones
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(null, "Hubo un problema al obtener los resultados de yearlyOverdue.");
+    }
+
+    return results;
+}
+   public void mostrarResultadosEnTabla(javax.swing.JTable table, List<Object[]> data, String[] columnNames) {
+        DefaultTableModel model = new DefaultTableModel(data.toArray(new Object[0][0]), columnNames);
+        int rowCount = model.getRowCount();         
+        int colCount = model.getColumnCount();         
+        for (int row = 0; row < rowCount; row++) {
+            for (int col = 0; col < colCount; col++) {
+                Object value = model.getValueAt(row, col);  
+                
+                if (col == 1) {
+
+                    model.setValueAt(value.toString().substring(0, 4), row, col);
+                }
+        
+                 
+        
+            }  
+        }
+        table.setModel(model);
+    
+}
+   public List<Object[]> mostrarResultadosYearlyOverdue() {
+    List<Object[]> yearlyOverdueResults = new ArrayList<>();
+
+    try {
+        // Lógica para obtener y mostrar los resultados de la consulta
+        yearlyOverdueResults = obtenerResultadosYearlyOverdue();
+
+        // Definir los nombres de las columnas
+        String[] columnNames = {"Costo", "Año"};
+
+        // Mostrar los resultados en jTable2
+        mostrarResultadosEnTabla(jTable2, yearlyOverdueResults, columnNames);
+
+    } catch (Exception e) {
+        // Manejo de excepciones
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(null, "Hubo un problema al obtener los resultados de yearlyOverdue.");
+    }
+
+    return yearlyOverdueResults;
+}
+public void actualizarYearlyOverdue(String year, String costo) {
+        try {
+            // Lógica para actualizar los datos en la base de datos
+            DatabaseConnection dbcn = new DatabaseConnection();
+            Connection cn = dbcn.getConnection();
+
+            String sql = "UPDATE yearlyOverdue SET cost = ? WHERE year = ?";
+            try (PreparedStatement pstmt = cn.prepareStatement(sql)) {
+                pstmt.setString(1, costo);
+                pstmt.setString(2, year);
+
+                int filasActualizadas = pstmt.executeUpdate();
+
+                if (filasActualizadas > 0) {
+                    JOptionPane.showMessageDialog(null, "Actualización exitosa.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se pudo actualizar el registro.");
+                }
+            }
+
+            cn.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Hubo un problema al actualizar los datos. Por favor, contacta al administrador.");
             System.out.println(e.getMessage());
         }
     }
